c9debd2a56b3ffcf4fe3fec090130609835bee72
Vivek Ragunathan <Vivek Ragunathan>
2017-03-19 21:30:30 -0700

    reorg

---
+++ App/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using Hugo.Data.Json;
+using System.Reflection;
+using System.IO;
+
+namespace Hugo.App
+{
+	public class Program
+	{
+		private const uint NO_OF_CLIENTS = 1000;
+
+
+		private static IEnumerable<Client> CreateClients(uint count)
+		{
+			for (int i = 0; i < count; i++)
+			{
+				yield return new Client
+				{
+					ClientId = i,
+					FirstName = "First Name " + i,
+					LastName = "Last Name " + i,
+					Email = "sender" + i + "@Example.com"
+				};
+			}
+		}
+
+		public static void Main(string[] args)
+		{
+			var jsonDbDirPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+			Console.WriteLine($"JSON Store Dir: {jsonDbDirPath}");
+
+			var sw = new Stopwatch();
+
+			sw.Start();
+			var itemStore = new JsonStore<Client>(new JsonDbCore(jsonDbDirPath, "hugo"));
+			sw.Stop();
+			Console.WriteLine("Initialized new Json store in {0} ms", sw.ElapsedMilliseconds);
+			itemStore.DeleteAll();
+
+			var clients    = CreateClients(NO_OF_CLIENTS);
+			var singleItem = clients.ElementAt(0);
+
+			sw.Reset();
+			sw.Start();
+			itemStore.Add(singleItem);
+			sw.Stop();
+			Console.WriteLine("Inserted single item in {0} ms", sw.ElapsedMilliseconds);
+
+			int savedId = singleItem.ClientId;
+
+			sw.Reset();
+			sw.Start();
+			var savedItems = itemStore.TryLoadData();
+			sw.Stop();
+			Console.WriteLine("Loaded {0} items in {1} ms", savedItems.Count, sw.ElapsedMilliseconds);
+
+			singleItem = savedItems.FirstOrDefault(itm => itm.ClientId == savedId);
+
+			singleItem.LastName = "Updated Last";
+			singleItem.FirstName = "Updated First";
+			singleItem.Email = "updated@example.com";
+
+			sw.Reset();
+			sw.Start();
+			itemStore.Update(singleItem);
+			sw.Stop();
+			Console.WriteLine("Updated single item in {0} ms", sw.ElapsedMilliseconds);
+
+			var notReferenceEqualItem = new Client
+			{
+				ClientId = singleItem.ClientId,
+				FirstName = "Copy First",
+				LastName = "Copy Last",
+				Email = "Copy Email"
+			};
+
+			sw.Reset();
+			sw.Start();
+			itemStore.Update(notReferenceEqualItem);
+			sw.Stop();
+			Console.WriteLine("Updated copy of single item in {0} ms", sw.ElapsedMilliseconds);
+
+			sw.Reset();
+			sw.Start();
+			savedItems = itemStore.TryLoadData();
+			sw.Stop();
+			Console.WriteLine("Loaded {0} items in {1} ms", savedItems.Count, sw.ElapsedMilliseconds);
+
+			singleItem = savedItems.FirstOrDefault(itm => itm.ClientId == savedId);
+
+			sw.Reset();
+			sw.Start();
+			itemStore.Delete(singleItem);
+			sw.Stop();
+			Console.WriteLine("Deleted single item in {0} ms", sw.ElapsedMilliseconds);
+
+			sw.Reset();
+			sw.Start();
+			itemStore.Add(clients);
+			sw.Stop();
+			Console.WriteLine("Inserted {0} item in {1} ms", savedItems.Count, sw.ElapsedMilliseconds);
+
+			sw.Reset();
+			sw.Start();
+			savedItems = itemStore.TryLoadData();
+			sw.Stop();
+			Console.WriteLine("Loaded {0} items in {1} ms", savedItems.Count, sw.ElapsedMilliseconds);
+
+			foreach (var item in savedItems)
+			{
+				int itemIndex = savedItems.IndexOf(item);
+				item.LastName = string.Format("Updated Last {0}", itemIndex);
+				item.FirstName = string.Format("Updated First {0}", itemIndex);
+				item.Email = string.Format("Updated email {0}", itemIndex);
+			}
+
+			sw.Reset();
+			sw.Start();
+			itemStore.Update(savedItems);
+			sw.Stop();
+			Console.WriteLine("Updated {0} items in {1} ms", savedItems.Count, sw.ElapsedMilliseconds);
+
+			sw.Reset();
+			sw.Start();
+			savedItems = itemStore.TryLoadData();
+			sw.Stop();
+			Console.WriteLine("Loaded {0} updated items in {1} ms", savedItems.Count, sw.ElapsedMilliseconds);
+
+			sw.Reset();
+			sw.Start();
+			itemStore.Delete(savedItems);
+			sw.Stop();
+			Console.WriteLine("Deleted {0} updated items in {1} ms", savedItems.Count, sw.ElapsedMilliseconds);
+
+			Console.Read();
+		}
+	}
+
+}
---
+++ Hugo.Data.Sqlite/SqliteDbCore.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Hugo.Core;
+using System.Data;
+using Microsoft.Extensions.Configuration;
+
+// This is unfortunate, but it works:
+#if __MonoCS__
+  using SQLiteConnectionAlias = Mono.Data.Sqlite.SqliteConnection;
+  using SQLiteCommandAlias = Mono.Data.Sqlite.SqliteCommand;
+  using SQLiteExceptionAlias = Mono.Data.Sqlite.SqliteException;
+#else
+
+using SQLiteConnectionAlias = Microsoft.Data.Sqlite.SqliteConnection;
+using SQLiteCommandAlias = Microsoft.Data.Sqlite.SqliteCommand;
+using SQLiteExceptionAlias = Microsoft.Data.Sqlite.SqliteException;
+
+#endif
+
+namespace Hugo.Data.Sqlite
+{
+	public class SqliteDbCore : DbCore
+	{
+		public override IDataStore<T> CreateRelationalStoreFor<T>()
+		{
+			return new SqliteRelationalStore<T>(this);
+		}
+
+		public override IDataStore<T> CreateDocumentStoreFor<T>()
+		{
+			return new SqliteDocumentStore<T>(this);
+		}
+
+		private string _defaultDbName = "data.db";
+
+		public string DBDirectory { get; set; }
+		public string DBName { get; set; }
+
+		public string DBFilePath
+		{
+			get { return Path.Combine(DBDirectory, DBName); }
+		}
+
+		public override string ConnectionString
+		{
+			get
+			{
+				return string.Format("Data Source = {0}", Path.Combine(DBDirectory, DBName));
+			}
+			set
+			{
+				//throw new ReadOnlyException("This property is Read Only in this implementation");
+				throw new Exception("This property is Read Only in this implementation");
+			}
+		}
+
+		public SqliteDbCore()
+		{
+			this.DBDirectory = this.GetDefaultDirectory();
+			this.DBName = _defaultDbName;
+			this.CreateDbIfNotExists();
+			this.LoadSchemaInfo();
+		}
+
+		public SqliteDbCore(string connectionStringName)
+		{
+			string connString = "";
+			try
+			{
+				var config = new ConfigurationBuilder()
+					.SetBasePath(Directory.GetCurrentDirectory())
+					.AddJsonFile("appsettings.json")
+					.Build();
+
+				connString = config.GetConnectionString(connectionStringName);
+				// connString = ConfigurationManager.ConnectionStrings[ connectionStringName ].ConnectionString;
+				string dbFileName = connString.Split('=').Last();
+				this.DBName = Path.GetFileName(dbFileName);
+				this.DBDirectory = Path.GetDirectoryName(dbFileName);
+			}
+			catch
+			{
+				this.DBDirectory = this.GetDefaultDirectory();
+				if (connectionStringName.Contains("."))
+				{
+					this.DBName = connectionStringName;
+				}
+				else
+				{
+					this.DBName = connectionStringName + ".db";
+				}
+			}
+			this.CreateDbIfNotExists();
+			this.LoadSchemaInfo();
+		}
+
+		public SqliteDbCore(string dbDirectory, string databaseName)
+		{
+			this.DBDirectory = dbDirectory;
+			this.DBName = databaseName;
+			this.CreateDbIfNotExists();
+			this.LoadSchemaInfo();
+		}
+
+		private string GetDefaultDirectory()
+		{
+			string defaultDirectory = "";
+			var currentDir = Directory.GetCurrentDirectory();
+			if (currentDir.EndsWith("Debug") || currentDir.EndsWith("Release"))
+			{
+				var projectRoot = Directory.GetParent(@"..\..\").FullName;
+				defaultDirectory = Path.Combine(projectRoot, "Data");
+			}
+			return defaultDirectory;
+		}
+
+		private void CreateDbIfNotExists()
+		{
+			string path = Path.Combine(this.DBDirectory, this.DBName);
+			if (!File.Exists(path))
+			{
+				Directory.CreateDirectory(this.DBDirectory);
+				using (File.Create(Path.Combine(this.DBDirectory, this.DBName))) { }
+				//SQLiteConnectionAlias.CreateFile(Path.Combine(this.DBDirectory, this.DBName));
+			}
+		}
+
+		public override bool TableExists(string tableName)
+		{
+			object result;
+			string sql = string.Format("select count(name) from sqlite_master where name = '{0}'", tableName);
+			using (var cn = new SQLiteConnectionAlias(this.ConnectionString))
+			{
+				using (var cmd = new SQLiteCommandAlias(sql, cn))
+				{
+					cn.Open();
+					try
+					{
+						result = cmd.ExecuteScalar();
+					}
+					catch (SQLiteExceptionAlias)
+					{
+						throw;
+					}
+					finally
+					{
+						cn.Close();
+					}
+				}
+			}
+			return (System.Int64) result > 0;
+		}
+
+		public override string DbDelimiterFormatString
+		{
+			get { return "\"{0}\""; }
+		}
+
+		protected override void LoadDbColumnsList()
+		{
+			this.DbColumnsList = new List<DbColumnMapping>();
+			var sqlPragma = "PRAGMA table_info({0})";
+			foreach (var tableName in DbTableNames)
+			{
+				var schemaSql = string.Format("SELECT 1 FROM sqlite_master WHERE tbl_name='{0}' and sql LIKE '%WITHOUT ROWID%'", tableName);
+				var noRowId = Convert.ToInt32(this.ExecuteScalar(schemaSql));
+
+				using (var dr = this.OpenReader(string.Format(sqlPragma, tableName)))
+				{
+					while (dr.Read())
+					{
+						var clm = dr[1] as string;
+						string dbType = dr.GetString(2);
+						bool isPk = dr.GetBoolean(5);
+						var newColumnMapping = new DbColumnMapping(this.DbDelimiterFormatString)
+						{
+							TableName = tableName,
+							ColumnName = clm,
+							PropertyName = clm,
+							IsPrimaryKey = dr.GetBoolean(5),
+							IsAutoIncementing = (isPk && (dbType.Equals("integer", StringComparison.CurrentCultureIgnoreCase)) && (noRowId == 0))
+						};
+						this.DbColumnsList.Add(newColumnMapping);
+					}
+				}
+			}
+		}
+
+		protected override void LoadDbTableNames()
+		{
+			this.DbTableNames = new List<string>();
+			var sql = "SELECT name from sqlite_master where type='table';";
+			using (var dr = this.OpenReader(sql))
+			{
+				while (dr.Read())
+				{
+					this.DbTableNames.Add(dr.GetString(0));
+				}
+			}
+		}
+
+		public override IDbConnection CreateConnection(string connectionString)
+		{
+			return new SQLiteConnectionAlias(connectionString);
+		}
+
+		public override IDbCommand CreateCommand()
+		{
+			return new SQLiteCommandAlias();
+		}
+	}
+}
